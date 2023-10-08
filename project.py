ebce6e34c77f6f6d1d10f0f0fee1cacd856b10b5
Oleksandr Kukhalskyy <olexandr.kukhalskyi@icloud.com>
2023-10-08 21:52:22 +0300

    New files/code

--- project.py
+++ project.py
@@ -1,17 +1,21 @@
+import sys
 from art import *
 from tabulate import tabulate
 
 
 def main():
     # Output programs logo/name in ascii format.
-    intro = welcome()
-    print(intro)
-    print(type(intro))
-
+    print(welcome())
     # Calling user to choose current type of the mood.
-    mood = get_mood()
-    print(mood)
-    print(type(mood))
+    mood: str = get_mood()
+    # Looking for the activities based on users mood.
+
+    if mood == "1":
+        ...  # Should implement function to look for 5 films for happy mood.
+    elif mood == "2":
+        ...  # Should implement function to look for 5 films for calm mood.
+    else:
+        ...  # Should implement function to look for 5 films for sad mood.
 
 
 def welcome():
@@ -19,22 +23,23 @@ def welcome():
     Converts program name into ascii art.
     :param: None
     :type: None
-    :raise: None
+    :raise: artError,...
     :return: Program name in ascii formatted art.
     :rtype: str
     """
 
     # Sets logo output name. Changeable - could be any other name.
     ascii_logo: str = "MOODIFY"
-
     # Sets font for logo name output. Changeable - look for 'art' library documentation.
-    ascii_font: str = "starwards"
-
+    ascii_font: str = "varsity"
     # Creating an object with attributes.
     intro = text2art(ascii_logo, ascii_font)
-
     # Returns str to main function.
     return intro
+
+
+def get_name():
+    ...
 
 
 def get_mood():
@@ -42,18 +47,33 @@ def get_mood():
     Calling user to make a choice of his current mood based on 3 options.
     :param: None
     :type: None
-    :raise: None
+    :raise: ...
     :return: User mood based on his choice.
     :rtype: str
     """
 
-    # Sets tabulation raws filled with the options that user should choose.
-    HEADERS: list = ["PRESS BUTTON:", "'1'", "'2'", "'3'"]
-    TABLE: list = [["IF YOU:", "HAPPY", "CALM", "SAD"]]
-
+    # Remains to automatically create columns based on the raw's values.
+    HEADERS: list = []
+    # For each row sets str with an option to choose from.
+    TABLE: list = [
+        ["PRESS BUTTON:", "1️⃣", "IF YOU FEELING", "😁", "HAPPY"],
+        ["PRESS BUTTON:", "2️⃣", "IF YOU FEELING", "😌", "CALM"],
+        ["PRESS BUTTON:", "3️⃣", "IF YOU FEELING", "😔", "SAD"],
+    ]
     # Sets tabulation font. Changeable - look for 'tabulate' library documentation.
-    TABLEFMT: str = "rounded_outline"
-    return tabulate(TABLE, HEADERS, tablefmt="rounded_outline")
+    TABLEFMT: str = "pretty"
+    # Outputs tabulated choices for user.
+    choices = tabulate(TABLE, HEADERS, TABLEFMT)
+    print(choices)
+
+    # Waiting for user to input valid value.
+    while True:
+        user_choice = input("\nEnter the number followed by 'Enter' button: ").strip()
+
+        if not user_choice or user_choice not in ["1", "2", "3"]:
+            print("\nPlease enter valid number:\n", choices)
+        else:
+            return user_choice
 
 
 if __name__ == "__main__":
